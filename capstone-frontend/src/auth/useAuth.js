bbcd14fa57034735c39ed65d62f6a2201fafca53
Ricky <gomez.ricky@gmail.com>
2026-02-15 19:27:56 -0600

    project cleanup

--- capstone-frontend/src/auth/useAuth.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { AuthContext } from "./AuthContextDef";
-
-export function useAuth() {
-  const context = useContext(AuthContext);
-  if (!context) throw Error("useAuth must be used within an AuthProvider");
-  return context;
-}
